19ca85126ef53f8bea1ae524d303c265ea7d72d6
SoftLeaderGy <826044698@qq.com>
2022-03-29 23:20:36 +0800

    初始化项目

---
+++ ud-file/src/main/java/com/yang/udfile/UdFileApplication.java
@@ -0,0 +1,13 @@
+package com.yang.udfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdFileApplication.class, args);
+    }
+
+}
